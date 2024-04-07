ab2e99abf1f814df55d0c6ab7167f2067b44db14
Ulllana <ulyanalana2003@gmail.com>
2024-04-07 14:54:14 +0300

    created gem

---
+++ test/test_minesweeper.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestMinesweeper < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Minesweeper::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
