c2bc4ffa79b80e449a6e763dd4f65fbc11a90e1a
BurnedTuner <ainol0912@gmail.com>
2024-04-07 15:14:47 +0300

    Board Initialization created

--- lib/minesweeper.rb
+++ lib/minesweeper.rb
@@ -16,7 +16,27 @@ module Minesweeper
   end
 
   class Board
-
+    attr_reader :width, :height, :num_mines
+  
+    def initialize(width, height, num_mines)
+      @width, @height, @num_mines = width, height, num_mines
+      @grid = Array.new(height) { Array.new(width) { Cell.new } }
+      place_mines
+      calculate_adjacent_mines
+    end
+  
+    def place_mines
+      placed_mines = 0
+      while placed_mines < @num_mines
+        rand_x = rand(@width)
+        rand_y = rand(@height)
+    
+        unless @grid[rand_y][rand_x].mine
+          @grid[rand_y][rand_x].mine = true
+          placed_mines += 1
+        end
+      end
+    end
   end
 
   class Game
